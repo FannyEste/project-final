2296a5a6aefca2c6462cdac1b519158ece7bc3f3
esstefanny <estefannymorana@gmail.com>
2025-01-31 08:25:45 +0100

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   base: '/', // Ensure correct asset paths
   css: {
     postcss: './postcss.config.cjs', // Ensure PostCSS config is linked
