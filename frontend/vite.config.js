f8657a2c4a9003b4f7446799165de418a2640072
esstefanny <estefannymorana@gmail.com>
2025-02-04 12:16:10 +0100

    try to fix css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // Ensure correct asset paths
-  css: {
-    postcss: './postcss.config.cjs', // Ensure PostCSS config is linked
-  },
+  //css: {
+  //  postcss: './postcss.config.cjs', // Ensure PostCSS config is linked
+ // },
   server: {
     historyApiFallback: true, // For React Router SPA
   },
