e444dd2cacb65c04db17a60150ebfcd20397aa7f
esstefanny <estefannymorana@gmail.com>
2025-02-05 18:54:19 +0100

    add missing import

--- frontend/src/assets/pages/Community.jsx
+++ frontend/src/assets/pages/Community.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/authContext";
 import "./Community.css";
 import { API_URL } from "../../config";
+import axios from "axios";
 
 const Community = () => {
     const { user, loading } = useAuth();
